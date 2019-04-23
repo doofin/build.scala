ce8b63061c809910a404f1c9c0a31bd92c8941dc
doofin <8177dph@gmail.com>
2019-04-23 14:54:25 +0800

    +

---
+++ build.sbt
@@ -0,0 +1,2 @@
+val p1 = build.exampleProject1
+val p2 = build.exampleProject2
--- project/build.scala
+++ project/build.scala
@@ -1,24 +1,21 @@
-
-
 //https://www.scala-sbt.org/0.13/docs/Full-Def.html
 //https://github.com/scalaz/scalaz/blob/series/7.3.x/project/build.scala/
 
 import sbt._
 import Keys._
 
-object ExampleBuild extends Build {
+object build {
 
   val dependencies = Seq(
-  )
+    )
 
-  lazy val exampleProject = Project("SbtExample", file(".")) settings(
-    version       := "0.2",
-    scalaVersion  := "2.12.8",
+  lazy val exampleProject1 = Project("exampleProject1", file(".")) settings (
+    version := "0.2",
+    scalaVersion := "2.12.8",
     scalacOptions := Seq("-deprecation"),
-    libraryDependencies ++= dependencies
+    libraryDependencies ++= dependencies,
+    addCommandAlias("c", ";compile")
   )
 
-  lazy val exampleProject1 = Project("p2", file("p2"))
-
+  lazy val exampleProject2 = Project("exampleProject2", file("p2"))
 }
-
